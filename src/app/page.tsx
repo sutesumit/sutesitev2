fafb78455a400c767d3a4d3b43b2cc6018814f31
sutesumit <sumitsute9@gmail.com>
2025-01-30 01:09:22 +0530

    page text updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,12 @@ export default function Home() {
         <p>Hellos.</p>
         <br/>
         <p>I transitioned from being an artist to a self-taught full-stack developer, but my goal remains the same—using creative mediums to tell stories and challenge perspectives. Now, I write code to build autonomous digital experiences—hoping to collaborate with others who seek to engineer ideas.</p>
+        <br/>
+        <p>Projects I am currently working on:</p>
+        <ul className="project-list">
+          <li>- art portfolio</li>
+          <li></li>
+        </ul>
       </div>
     </article>
   );
