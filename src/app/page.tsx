88a9f637ef20552a86bec2e67f46b7101e33a04a
sutesumit <sumitsute9@gmail.com>
2025-01-30 00:57:50 +0530

    modified page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="p-10 container flex items-center h-screen font-roboto-mono">
-      Hellos. This is Sumit Sute's Site. Version 2.
-    </div>
+    <article className="p-10 container flex items-center h-screen font-roboto-mono lowercase">
+      <div>
+        <p>Hellos.</p>
+        <br/>
+        <p>I transitioned from being an artist to a self-taught full-stack developer, but my goal remains the same—using creative mediums to tell stories and challenge perspectives. Now, I write code to build autonomous digital experiences—hoping to collaborate with others who seek to engineer ideas.</p>
+      </div>
+    </article>
   );
 }
