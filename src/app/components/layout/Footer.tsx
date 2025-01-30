98572e77bb6dc040d3986e7a39a89dfa0e55176e
sutesumit <sumitsute9@gmail.com>
2025-01-30 17:02:26 +0530

    clock added to page

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,15 +1,22 @@
+'use client'
 import React from 'react'
 
-
-
-
-
 const Footer = () => {
+  const [currentTime, setCurrentTime] = React.useState(new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds())
+
+  React.useEffect(() => {
+    const interval =setInterval(() => {
+      setCurrentTime(new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds())
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <footer className="footer fixed w-full bottom-0">
       <div className='container flex justify-between p-1 border-t-2 border-slate-100 lowercase opacity-50 hover:opacity-70 '>
           <div className="current-time text-xs">
-               {new Date().toLocaleString()}
+               {currentTime}
           </div>
           <div className="last-visit text-xs">
                 Bengaluru, IN
