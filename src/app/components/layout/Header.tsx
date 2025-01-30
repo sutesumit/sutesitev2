ae2402a89d5de3bbb13a0e06ee2b228ca0de5abd
sutesumit <sumitsute9@gmail.com>
2025-01-30 17:19:19 +0530

    theme toggler state enabled

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,9 +1,13 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import { FaMoon } from "react-icons/fa";
 import { ImSun } from 'react-icons/im';
 
 const Header = () => {
+
+  const [darkMode, setDarkMode] = React.useState(false)
+
   return (
     <header className='header fixed w-full'>
       <nav className='navbar container flex justify-between items-baseline p-2 border-b-2 border-slate-100 lowercase'>
@@ -14,9 +18,10 @@ const Header = () => {
           <li><Link href="/" className='opacity-50 hover:opacity-70'>Work</Link></li>
           <li><Link href="/" className='opacity-50 hover:opacity-70'>About</Link></li>
           <li className="toggleTheme opacity-50 hover:opacity-70 cursor-pointer text-xs">
-            <button className='flex items-center gap-1'>
-              <FaMoon/>
-              <ImSun/>
+            <button className='flex items-center gap-1' 
+                    onClick={() => setDarkMode(!darkMode)}
+            >
+              {darkMode ? <ImSun/> : <FaMoon/>}
             </button>
           </li>
         </ul>
