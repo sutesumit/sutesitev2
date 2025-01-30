1bc628bea1834bd4b99ccda5812b691a99d5285a
sutesumit <sumitsute9@gmail.com>
2025-01-30 12:55:36 +0530

    bio text changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ export default function Home() {
   return (
     <article className="p-10 container flex items-center h-screen font-roboto-mono lowercase">
       <div>
-        <p>Hellos.</p>
+        <p>Hellos. Sumit Sute here. </p>
         <br/>
-        <p>I&apos;m a full-stack developer, self-taught and deeply influenced by my practice as an artist, still driven by the goal of using creative mediums to tell stories and challenge perspective.</p>
+        <p>I'm a full-stack developer, self-taught and deeply influenced by my practice as an artist.</p>
+        <br/>
+        <p>I create interactive digital art exploring the intersection of personal archives, inherited emotions, and the political.  My work, informed by the feminist principle that the personal is political, imagines web technologies as a medium for artistic expression and self-reflection.</p>
         <br/>
         <p>Projects I am currently working on:</p>
         <ul className="project-list">
