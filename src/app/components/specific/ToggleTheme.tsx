4ebbc2944543d6f4fa67979ea2910986e8bb406b
sutesumit <sumitsute9@gmail.com>
2025-02-15 23:19:01 +0530

    motion variants used

--- src/app/components/specific/ToggleTheme.tsx
+++ src/app/components/specific/ToggleTheme.tsx
@@ -9,6 +9,17 @@ const ToggleTheme = () => {
     const sunPath =  "M11 15C13.2091 15 15 13.2091 15 11C15 8.79086 13.2091 7 11 7C8.79086 7 7 8.79086 7 11C7 13.2091 8.79086 15 11 15Z"
     const moonPath = "M11 15C13.2091 15 15 13.2091 15 11C10.3643 13.1535 9.59148 10.4046 11 7C8.79086 7 7 8.79086 7 11C7 13.2091 8.79086 15 11 15Z" 
 
+    const rayVarients = {
+        hidden: { opacity: 0, pathLength: 0 },
+        visible: {
+            opacity: theme === 'light' ? 1 : 0,
+            pathLength: 1,
+            transition: {
+                staggerChildren: 0.1,
+                staggerDirection: -1
+            }
+        }
+    }
 
     React.useEffect(() => {
         setMounted(true)
@@ -27,37 +38,37 @@ const ToggleTheme = () => {
                 <m.svg className='relative overflow-visible' width="16" height="16" viewBox="0 0 22 22" fill="currentColor" stroke="currentColor" xmlns="http://www.w3.org/2000/svg"
                     initial={{ scale: 0, opacity: 0, rotate: 0 }}
                     animate={{ 
-                        scale: theme === 'dark' ? 1.1 : 1.5, 
+                        scale: theme === 'dark' ? 1.5 : 1.1, 
                         opacity: 1,
                         rotate: theme === 'dark' ? 0 : 360,
+                        transition: { duration: 0.3, ease: "easeInOut" }                        
                     }}
-                    transition={{ duration: 0.3 }}
+                    
                 >
                     <m.path className='absolute inset-0' stroke="currentColor" strokeWidth="1"
                         initial={{ scale: 0, opacity: 0, rotate: 0 }} 
                         animate={{ 
                             scale: 1, 
                             opacity: 1,
-                            d: theme === 'dark' ? sunPath : moonPath,
+                            d: theme === 'dark' ? moonPath : sunPath,
                             transition: { duration: 0.3, ease: "easeInOut" }
                         }} 
                     />
                     <m.g
                         strokeWidth="2"
-                        initial={{ scale: 0, opacity: 0 }}
-                        animate={{ 
-                            scale: theme === 'dark' ? 1 : 0, 
-                            opacity: theme === 'dark' ? 1 : 0 }}
-                        transition={{ staggerChildren: 0.1, duration: 0.3 }}
+                        variants={rayVarients}
+                        initial="hidden"
+                        animate="visible"
                     >
-                        <path d="M11 1V3"/>
-                        <path d="M11 19V21"/>
-                        <path d="M3.93 3.93L5.34 5.34"/>
-                        <path d="M16.66 16.66L18.07 18.07"/>
-                        <path d="M1 11H3"/>
-                        <path d="M19 11H21"/>
-                        <path d="M5.34 16.66L3.93 18.07"/>
-                        <path d="M18.07 3.93L16.66 5.34"/>
+                        <m.path  variants={rayVarients} initial="hidden" animate="visible" d="M11 1V3"/>
+                        <m.path variants={rayVarients} initial="hidden" animate="visible" d="M18.07 3.93L16.66 5.34"/>
+                        <m.path variants={rayVarients} initial="hidden" animate="visible" d="M19 11H21"/>
+                        <m.path variants={rayVarients} initial="hidden" animate="visible" d="M16.66 16.66L18.07 18.07"/>
+                        <m.path variants={rayVarients} initial="hidden" animate="visible" d="M11 19V21"/>
+                        <m.path variants={rayVarients} initial="hidden" animate="visible" d="M5.34 16.66L3.93 18.07"/>
+                        <m.path variants={rayVarients} initial="hidden" animate="visible" d="M1 11H3"/>
+                        <m.path variants={rayVarients} initial="hidden" animate="visible" d="M3.93 3.93L5.34 5.34"/>
+                        
                     </m.g>
                 </m.svg>
             </button>
