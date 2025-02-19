0771c3aa699f672483a7c51509ea990aacf7af1f
sutesumit <sumitsute9@gmail.com>
2025-02-19 16:15:50 +0530

    hover icon-popup implemented to about page

---
+++ src/app/about/bullets.ts
@@ -0,0 +1,76 @@
+
+
+const bullets = [
+    {
+        body: "Reimagined my lens-based visual art (photo objects) as web experiences, bridging the personal and the political.",
+        icons: ["🌐"],
+        time: "2025"
+    },
+    {
+        body: "Developed Dramas of Discrimination, a web application facilitating workshops for diverse communities, integrating systems thinking and Theatre of the Oppressed methodologies.",
+        icons: ["📚"],
+        time: "2025"
+    },
+    {
+        body: "Married Pallavi, the love of my life. We have four kids now.",
+        icons: ["👪"],
+        time: "2024"
+    },
+    {
+        body: "Awarded the Mirror&apos;s Fellowship for creative expressions on the (de)construction of masculinity.",
+        icons: ["🏆"],
+        time: "2024"
+    },
+    {
+        body: "Represented Ambedkar Reading Circle (arc) at the Second Global Conference on Caste, Business, and Society at the University of Bath, UK.",
+        icons: ["🏛"],
+        time: "2024"
+    },
+    {
+        body: "Took a sabbatical to care for my mother during a health relapse—began coding at her hospital bedside.",
+        icons: ["📚"],
+        time: "2024"
+    },
+    {
+        body: "Contributed to &apos;All That Blue&apos; magazine, curating anti-caste creative expressions from India.",
+        icons: ["📚"],
+        time: "2024"
+    },
+    {
+        body: "Initiated community organizing and program design at the Ambedkar Reading Circle, making anti-caste philosophy and literature more accessible in public spaces.",
+        icons: ["🧝‍♂️"],
+        time: "2023"
+    },
+    {
+        body: "Exhibited at the Vannam Photo Festival in Chennai.",
+        icons: ["🎭"],
+        time: "2023"
+    },
+    {
+        body: "Moved to Bangalore to work as a communication strategist at a games for public policy startup.",
+        icons: ["📚"],
+        time: "2022"
+    },
+    {
+        body: "Took a sabbatical to care for my mother, deepening my relationship with my father.",
+        icons: ["👪"],
+        time: "2022"
+    },
+    {
+        body: "Relocated to New Delhi to work as a photo editor at Hindustan Times.",
+        icons: ["📚"],
+        time: "2021"
+    },
+    {
+        body: "Worked as a documentary photographer for governmental and non-governmental organizations, including the Maharashtra Government, Paani Foundation, Indian Institute of Human Settlements, and The Wire, covering rural and urban stories across Maharashtra, Gujarat, and Karnataka.",
+        icons: ["📚"],
+        time: "2018"
+    },
+    {
+        body: "Completed my Dual Degree Thesis, &apos;Robot Motion Planning using Derivatives of Rapidly Exploring Random Trees&apos;, at IIT Madras, earning both Bachelor&apos;s and Master&apos;s degrees in Mechanical Engineering.",
+        icons: ["📚"],
+        time: "2018"
+    }
+]
+
+export default bullets
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,16 @@
 'use client'
-
+import React from 'react'
 import dynamic from 'next/dynamic'
+import bullets from './bullets'
+
 const FallingLeaves = dynamic(() => import('../components/specific/FallingLeaves'), {
   ssr: false,
 })
 
 export default function Home() {
+
+  const [ showIcon, setShowIcon ] = React.useState(false)
+
   return (
     <article className="p-10 container h-auto items-center font-roboto-mono lowercase">
       <div className="h-[calc(100vh-10rem)] pt-5">
@@ -16,22 +21,18 @@ export default function Home() {
         <p>Before finding my rightful place in web development, my path has taken me through mechanical engineering, documentary photography, editorial journalism, communication strategy, and community organizing. A common thread runs through all these experiences: the drive to create things that enhance and complement life.</p>
         <br/> 
         <p className="inline">Building backwards and retracing every step that led me here.</p><div className="inline"><FallingLeaves /></div>
-        <ul className='life-line project-list p-2 pb-10'>
-          <li>Reimagined my lens-based visual art (photo objects) as web experiences, bridging the personal and the political.</li>
-          <li>Developed Dramas of Discrimination, a web application facilitating workshops for diverse communities, integrating systems thinking and Theatre of the Oppressed methodologies.</li>
-          <li>Married Pallavi, the love of my life. We have four kids now.</li>
-          <li>Awarded the Mirror&apos;s Fellowship for creative expressions on the (de)construction of masculinity.</li>
-          <li>Represented Ambedkar Reading Circle (arc) at the Second Global Conference on Caste, Business, and Society at the University of Bath, UK.</li>
-          <li>Took a sabbatical to care for my mother during a health relapse—began coding at her hospital bedside.</li>
-          <li>Contributed to &apos;All That Blue&apos; magazine, curating anti-caste creative expressions from India.</li>
-          <li>Initiated community organizing and program design at the Ambedkar Reading Circle, making anti-caste philosophy and literature more accessible in public spaces.</li>
-          <li>Exhibited at the Vannam Photo Festival in Chennai.</li>
-          <li>Moved to Bangalore to work as a communication strategist at a games for public policy startup.</li>
-          <li>Took a sabbatical to care for my mother, deepening my relationship with my father.</li>
-          <li>Relocated to New Delhi to work as a photo editor at Hindustan Times.</li>
-          <li>Worked as a documentary photographer for governmental and non-governmental organizations, including the Maharashtra Government, Paani Foundation, Indian Institute of Human Settlements, and The Wire, covering rural and urban stories across Maharashtra, Gujarat, and Karnataka.</li>
-          <li>Completed my Dual Degree Thesis, &apos;Robot Motion Planning using Derivatives of Rapidly Exploring Random Trees&apos;, at IIT Madras, earning both Bachelor&apos;s and Master&apos;s degrees in Mechanical Engineering.</li>
-       </ul>
+        <ul className="life-line project-list p-2 pb-10">
+          {bullets.map((bullet, index) => (
+            <li 
+              key={index}
+              className='relative'
+            >
+              <span className={`absolute h-full w-full left-2 opacity-0 hover:opacity-100 transition-all duration-300`}>{bullet.icons}</span>
+              <br/>
+              {bullet.body}
+            </li>
+          ))}
+        </ul>
       </div>
     </article>
   );
