3f7be943ef4b6a607573c9f7d92a811d591b96c9
sutesumit <sumitsute9@gmail.com>
2025-02-26 19:30:38 +0530

    socials added to footer

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -1,5 +1,8 @@
 'use client'
 import React from 'react'
+import Link from 'next/link'
+import { FaGithub, FaTwitter } from 'react-icons/fa'
+import { IoIosMail } from 'react-icons/io'
 
 const Footer = () => {
   const [currentTime, setCurrentTime] = React.useState('--:--:--')
@@ -18,10 +21,12 @@ const Footer = () => {
   return (
     <footer className="footer fixed w-full bottom-0 z-10">
       <div className='container flex justify-between p-2 border-t-2 border-slate-300 dark:border-slate-700 backdrop-blur-3xl lowercase'>
-          <div className="current-time text-xs opacity-50 hover:opacity-70">
-               {currentTime}
+          <div className="current-time flex items-center gap-3">
+            <Link className='opacity-50 hover:opacity-70' target='_blank' href='https://github.com/sutesumit'><FaGithub /></Link> 
+            <Link className='opacity-50 hover:opacity-70' target='_blank' href='https://x.com/sutenet'><FaTwitter /></Link> 
+            <Link className='opacity-50 hover:opacity-70' target='_blank' href='mailto:sumitsute@alumni.iitm.ac.in'><IoIosMail /></Link>
           </div>
-          <div className="last-visit text-xs opacity-50 hover:opacity-70">
+          <div className="last-visit flex items-center gap-1 text-xs opacity-50 hover:opacity-70">
                 Bengaluru, IN
           </div>
       </div>
