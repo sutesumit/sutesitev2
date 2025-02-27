81371fea5993800d1ac2f7d8da3fa18b46606c7a
sutesumit <sumitsute9@gmail.com>
2025-02-27 23:49:22 +0530

    accordion made client component

--- src/app/work/ProjectPage.tsx
+++ src/app/work/ProjectPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import LiveRepoLinks from '../components/shared/LiveRepoLinks';
-import Accordian from '../components/shared/Accordian';
+import Accordion from '../components/shared/Accordion';
 import { projects, ProjectProps } from './projectlist';
 
 
@@ -16,26 +16,26 @@ const ProjectPage = ({project}: { project: ProjectProps}) => {
             <div className='about-section my-2 md:pl-8'>
               <div className='project-description py-1'>{project.description}</div>
               <LiveRepoLinks livelink={project.livelink} repolink={project.githublink} />              
-              <Accordian title='About'>{project.about}</Accordian>
-              {/* <Accordian title="Features">
+              <Accordion title='About'>{project.about}</Accordion>
+              {/* <Accordion title="Features">
                 {project.features.map((feature, index) => (
                   <div key={index}>{feature}</div>
                 ))}
-              </Accordian> */}
-              <Accordian title='Tech Stack'>
+              </Accordion> */}
+              <Accordion title='Tech Stack'>
                 <div className='project-technologies flex gap-2'>
                   {project.technologies.map((tech, index) => (
                     <span className='tab text-xs flex items-center gap-1 tech-keyword' key={index}>{tech}</span>
                   ))}
                 </div>
-              </Accordian>
-              <Accordian title='Roles'>
+              </Accordion>
+              <Accordion title='Roles'>
                 <div className='project-roles flex gap-2'>
                   {project.roles.map((role, index) => (
                     <span className='tab text-xs flex items-center gap-1 role-keyword' key={index}>{role}</span>
                   ))}
                 </div>
-              </Accordian>
+              </Accordion>
             </div>       
 
         </ul>
