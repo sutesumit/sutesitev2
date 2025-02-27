48a80fce782232541fd83cd3026c3955bd4ecde2
sutesumit <sumitsute9@gmail.com>
2025-02-27 23:43:23 +0530

    linting errors fixed

--- src/app/work/art/page.tsx
+++ src/app/work/art/page.tsx
@@ -1,11 +1,8 @@
-'use client'
 import React from 'react'
-import { usePathname } from 'next/navigation'
 import { projects } from '../projectlist'
 import ProjectPage from '../ProjectPage';
 
 const page = () => {
-    const pathname = usePathname();
     const project = projects[0];
     return (
       <ProjectPage project={project} />
