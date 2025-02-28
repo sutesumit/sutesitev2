824384c91ceb8d645d18132e7905042dccde34aa
sutesumit <sumitsute9@gmail.com>
2025-02-28 17:37:38 +0530

    skillset object refactored

--- src/data/skilllist.tsx
+++ src/data/skilllist.tsx
@@ -1,8 +1,7 @@
 import { FaCss3 } from 'react-icons/fa';
-import { SiFigma, SiJavascript, SiHtml5, SiTypescript, SiPython, SiReact, SiNextdotjs, SiNodedotjs, SiExpress, SiTailwindcss, SiFramer, SiMongodb, SiDocker } from 'react-icons/si';
+import { SiFigma, SiJavascript, SiHtml5, SiTypescript, SiPython, SiReact, SiNextdotjs, SiNodedotjs, SiExpress, SiTailwindcss, SiFramer, SiMongodb } from 'react-icons/si';
 import { BiLogoPostgresql } from 'react-icons/bi';
 import { FaGitAlt } from 'react-icons/fa6';
-import { VscVscode } from 'react-icons/vsc';
 
 export const skillList = {
     languages: [
@@ -12,22 +11,22 @@ export const skillList = {
         { name: 'HTML', icon: <SiHtml5 /> },
         { name: 'CSS', icon: <FaCss3 /> },
     ],
-    frameworks: [
+    frontend: [
         { name: 'React', icon: <SiReact /> },
-        { name: 'Next.js', icon: <SiNextdotjs /> },
-        { name: 'Node.js', icon: <SiNodedotjs /> },
-        { name: 'Express.js', icon: <SiExpress /> },
         { name: 'Tailwind', icon: <SiTailwindcss /> },
         { name: 'Motion', icon: <SiFramer /> },
+    ],
+    backend: [
+        { name: 'Node.js', icon: <SiNodedotjs /> },
+        { name: 'Express.js', icon: <SiExpress /> },
     ],
     databases: [
         { name: 'MongoDB', icon: <SiMongodb /> },
         { name: 'PostgreSQL', icon: <BiLogoPostgresql /> },
     ],
     tools: [
+        { name: 'Next.js', icon: <SiNextdotjs /> },
         { name: 'Git', icon: <FaGitAlt /> },
-        { name: 'Docker', icon: <SiDocker /> },
-        { name: 'VSCode', icon: <VscVscode /> },
         { name: 'Figma', icon: <SiFigma /> },
     ],
     stacks: [
