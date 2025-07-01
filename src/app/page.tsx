bff93755f19840cfe40d9b1d22ea2b1e8bdd2d84
sutesumit <sumitsute9@gmail.com>
2025-07-01 23:06:55 +0530

    update(workplace): beneathAtree link on a new page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <p>Hellos. Sumit Sute here.</p><SeedingPlant/>
         </div>
         <br/>
-        <p>From working as an artist to now building software at <Link className='border-[1px] border-opacity-25 dark:border-opacity-25 bg-blue-100 dark:bg-blue-950 border-blue-900 dark:border-blue-400 rounded-sm px-1' href='https://beneathatree.com/'>beneathAtree</Link>, my creative practice continues to shape how I write and think about code.</p>
+        <p>From working as an artist to now building software at <Link target="_blank" className='border-[1px] border-opacity-25 dark:border-opacity-25 bg-blue-100 dark:bg-blue-950 border-blue-900 dark:border-blue-400 rounded-sm px-1' href='https://beneathatree.com/'>beneathAtree</Link>, my creative practice continues to shape how I write and think about code.</p>
         <br/>
         <p>I create interactive digital art exploring the intersection of personal archives, inherited emotions, and the political.  My work, informed by the feminist principle that the personal is political, imagines web technologies as a medium for artistic expression and self-reflection.</p>
         <br/>
