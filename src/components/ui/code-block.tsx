cfe07c53c502d0e5c21faabba00353e3dc797123
sutesumit <sumitsute9@gmail.com>
2025-12-20 14:28:58 +0530

    update/newMetadata: updated CodeBlock bg for light mode

--- src/components/ui/code-block.tsx
+++ src/components/ui/code-block.tsx
@@ -54,7 +54,7 @@ export const CodeBlock = ({
     : highlightLines;
 
   return (
-    <div className="relative w-full rounded-lg bg-slate-900 p-4 font-mono text-sm mb-4">
+    <div className="relative w-full rounded-lg bg-slate-800 dark:bg-slate-900 p-4 font-mono text-sm mb-4">
       <div className="flex flex-col gap-2">
         {tabsExist && (
           <div className="flex  overflow-x-auto">
